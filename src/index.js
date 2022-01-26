423bf5b5f86b4461c25b084297cb80d89ec47907
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-26 16:43:57 -0600

    Add ability to add new scores, and store the list of scores into the local storage. Used classes separated into modules.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './cr_icon.jpg';
-import printMe from './print.js';
+// import printMe from './print.js';
+import ScoresList from './modules/ScoresList';
+
+const scoresList = new ScoresList();
+
+const addScoreForm = document.getElementById('scores-form');
+addScoreForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    scoresList.addScore();
+});
+const addScoreButton = document.getElementById('add-score');
+addScoreButton.addEventListener('click', () => scoresList.addScore());
+
+scoresList.displayList();
 
 
-function component() {
+
+
+/* function component() {
     const element = document.createElement('div');
     const btn = document.createElement('button');
 
@@ -16,12 +30,7 @@ function component() {
 
     element.appendChild(btn);
 
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
     return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component()); */
