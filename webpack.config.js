5dad0dfb9e51c2dd3b09824d766f789e14cdfa09
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-27 14:55:25 -0600

    Install babel for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,16 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ],
   },
   output: {
