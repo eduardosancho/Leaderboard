89969c4087527deef992286662229cc60fd02d31
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-28 14:08:46 -0600

    Run linters check

--- src/modules/showSuccess.js
+++ src/modules/showSuccess.js
@@ -1,13 +1,12 @@
 import 'babel-polyfill';
-import { set } from 'lodash';
 
 const showSuccess = (message) => {
-    const msgContainer = document.querySelector('#scores-form span');
-    msgContainer.textContent = `${message}`;
-    msgContainer.classList.remove('hidden');
-    setTimeout(function () {
-        msgContainer.classList.add('hidden');
-    }, 2000);
-}
+  const msgContainer = document.querySelector('#scores-form span');
+  msgContainer.textContent = `${message}`;
+  msgContainer.classList.remove('hidden');
+  setTimeout(() => {
+    msgContainer.classList.add('hidden');
+  }, 2000);
+};
 
 export default showSuccess;
